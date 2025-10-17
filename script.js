d5d2fc8ca69ce477d9bdf5b83f751a6d4aa636bc
saak <31166582+psyberien@users.noreply.github.com>
2025-10-17 23:38:54 +0800

    Day2

--- script.js
+++ script.js
@@ -6,17 +6,15 @@ console.log("hisashiburi dana")
 // returns true when passed as parameter to the callback 
 // function
 
-let allAreLessThanSeven = all([1,2,9], function(num){
-	return num < 7;
+let allAreLessThanSeven = all([1, 2, 8], function(num) {
+    return num < 7;
 });
-
 console.log(allAreLessThanSeven);
-
-function all(arr, callback) {
+function all(arr, callback){
     if(arr.length === 0) return true;
     const copy = arr.slice();
 
-    if(callback(copy[0])) {
+    if(callback(copy[0])){
         copy.shift();
         return all(copy, callback);
     }
@@ -26,14 +24,17 @@ function all(arr, callback) {
 // Question 2: Product of an array
 // Write a function called productOfArray which takes in 
 // an array of numbers and returns the product of them all
-
+// let six = productOfArray([1,2,3]) // 6
+// let sixty = productOfArray([1,2,3,10]) // 60
 let six = productOfArray([1,2,3]) // 6
 let sixty = productOfArray([1,2,3,10]) // 60
 console.log(six, sixty);
-function productOfArray(arr){   
+
+function productOfArray(arr) {
     if(arr.length === 0) return 1;
     return arr.shift() * productOfArray(arr);
 }
+
 
 // Question 3: Search JS object
 // Write a function called contains that searches for a 
@@ -54,57 +55,62 @@ let nestedObject = {
         }
     }
 }
-
+// let hasIt = contains(nestedObject, 44); // true
+// let doesntHaveIt = contains(nestedObject, "foo"); // false
 let hasIt = contains(nestedObject, 44); // true
 let doesntHaveIt = contains(nestedObject, "foo"); // false
-console.log(hasIt, doesntHaveIt);
 
+console.log(hasIt, doesntHaveIt);
 function contains(object, searchValue) {
     if(typeof object !== "object" || object === null) {
         return object === searchValue;
     }
 
     for(const value of Object.values(object)) {
-        if(contains(value, searchValue)) {
+        if(contains(value ,searchValue)) {
             return true;
         }
     }
     return false;
 }
 
+
+
 // Question 4: Parse a multi-dimensional array
 // Given a multi-dimensional integer array, return the 
 // total number of integers stored inside this array
+// let seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
 let seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
 console.log(seven);
 
 function totalIntegers(arr) {
     if(arr.length === 0) return 0;
-
     let total = 0;
     let first = arr.shift();
 
-    if(Array.isArray(first)) {
+    if(Array.isArray(first)){
         total+= totalIntegers(first);
     } else if(Number.isInteger(first)) {
-        total += 1;
+        total+= 1;
     }
 
     return total + totalIntegers(arr);
- }
+}
 
  // Question 5:
 // Write a function that sums squares of numbers in list 
 // that may contain more lists
-
-
+// console.log(SumSquares([1,2,3])); // 14 
+// console.log(SumSquares([[1,2],3])); // 14
+// console.log(SumSquares([[[[[[[[[1]]]]]]]]])); // 1
+// console.log(SumSquares([10,[[10],10],[10]])); // 400
 console.log(SumSquares([1,2,3])); // 14 
 console.log(SumSquares([[1,2],3])); // 14
 console.log(SumSquares([[[[[[[[[1]]]]]]]]])); // 1
 console.log(SumSquares([10,[[10],10],[10]])); // 400
 
 function SumSquares(arr) {
-    if(arr.length === 0) return 0;
+    if(arr.length === 0 ) return 0;
     let total = 0;
 
     for(let i=0; i<arr.length; i++) {
@@ -117,17 +123,22 @@ function SumSquares(arr) {
     return total;
 }
 
+
+
 // Question 6:
 // The function should return an array containing 
 // repetitions of the number argument. For instance, 
 // replicate(3, 5) should return [5,5,5]. If the times 
 // argument is negative, return an empty array.
+// console.log(replicate(3, 5)) // [5, 5, 5]
+// console.log(replicate(1, 69)) // [69]
+// console.log(replicate(-2, 6)) // []
 
 console.log(replicate(3, 5)) // [5, 5, 5]
 console.log(replicate(1, 69)) // [69]
 console.log(replicate(-2, 6)) // []
 
-function replicate(times, number){
+function replicate(times, number) {
     if(times <= 0) return [];
 
     return [number].concat(replicate(times - 1, number));
